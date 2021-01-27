7839be80c2f784d0c97afc71e04694790241853e
John DeVoe <john.devoe@users.noreply.github.com>
2021-01-26 22:42:29 -0500

    All of Work at home on 1/26
    Added basic movement, level-building, NPC movement and detection, and night-vision

--- Characters/TemplateCharacter.gd
+++ Characters/TemplateCharacter.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
 
-const SPEED = 10
-const MAX_SPEED = 100
-const FRICTION = 0.1
+const SPEED = 25
+const MAX_SPEED = 225
+const FRICTION = 0.07
---
+++ Interface/VisionMode.gd
@@ -0,0 +1,30 @@
+extends CanvasModulate
+
+const DARK = Color("0e0e0e")
+const NIGHTVISION = Color("37bf62")
+
+var sound_cooldown = false
+
+func _ready():
+	color = DARK
+	visible = true
+
+
+func cycle_vision_mode():
+	if color == DARK and not sound_cooldown:
+		sound_cooldown = true
+		color = NIGHTVISION
+		$AudioStreamPlayer.stream = load("res://SFX/nightvision.wav")
+		$AudioStreamPlayer.play()
+		$Timer.start()
+		
+	if color == NIGHTVISION and not sound_cooldown:
+		sound_cooldown = true
+		color = DARK
+		$AudioStreamPlayer.stream = load("res://SFX/nightvision_off.wav")
+		$AudioStreamPlayer.play()
+		$Timer.start()
+
+
+func _on_Timer_timeout():
+	sound_cooldown = false
